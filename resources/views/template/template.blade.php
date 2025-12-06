e236e40d4af7206c7879e5fe14fcd029686693d0
idn-fahmifn <fahmifn.idn@gmail.com>
2025-12-06 13:53:25 +0700

    final

--- resources/views/template/template.blade.php
+++ resources/views/template/template.blade.php
@@ -23,10 +23,10 @@
                         <a class="nav-link active" aria-current="page" href="#">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Data Kategori</a>
+                        <a class="nav-link" href="{{ route('category.index') }}">Data Kategori</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Data Produk</a>
+                        <a class="nav-link" href="{{ route('product.index') }}">Data Produk</a>
                     </li>
                 </ul>
             </div>
